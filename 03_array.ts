f7f4aabe07b1cd4ee3ce700a9a206d08c0e211bf
ashishpawar <ashishpawar8077@gmail.com>
2025-05-24 21:16:23 +0530

    Arrays in typescript

---
+++ 03_array.ts
@@ -0,0 +1,26 @@
+
+//*Array*
+
+// TypeScript, arrays are a type of object used to store multiple items of similar (or explicitly defined) data types
+
+let fruits : string[] = ["Mango", "Orange", "Banana"]
+fruits.push("Apple")
+//fruits.push(2)  ❌ Error:  Argument of type 'number' is not assignable to parameter of type 'string'
+
+// Alternative syntax for creating array in typescript 
+const webTech:Array<string> = ["Javascript", "typscript", "Nodejs", "Express"]
+
+
+// *MultiDimensional*
+
+const MultiDimensional:number[][] = [[5,6,7,8],[1,2,3,4,5,6,8]] 
+const tripleDimensional:number[][][] = [[[5,6,7,8],[1,2,3,4,5,6,8]]]
+
+
+
+
+
+
+
+
+
